dec984f0e7277445d43edd418af0615b59527898
Oren Fromberg <oren@adhocteam.us>
2020-04-06 12:41:23 -0400

    Refactoring script

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 
-# build
-docker-compose -f docker-compose.yml build static_site
+set -eo pipefail
 
-# test
-docker-compose run -u $(id -u ${USER}):$(id -g ${USER}) --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
-sleep 20
-curl localhost:4000 | grep "Join the Google Group"
-docker-compose down
+docker_cleanup()
+{
+    docker-compose down
+}
+
+build() {
+    docker-compose -f docker-compose.yml build static_site
+}
+
+test() {
+    trap docker_cleanup EXIT
+    docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
+    sleep 20
+    curl localhost:4000 | grep "Join the Google Group"
+}
+
+main() {
+    build
+    test
+}
+
+main
