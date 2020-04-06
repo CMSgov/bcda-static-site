21885401d457886b4c0fa851cbf30bac99d95a46
Oren Fromberg <oren@adhocteam.us>
2020-04-06 12:52:34 -0400

    breaking test on purpose

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -15,7 +15,7 @@ test() {
     trap docker_cleanup EXIT
     docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
     sleep 20
-    curl localhost:4000 | grep "Join the Google Group"
+    curl localhost:4000 | grep "blah blah blah"
 }
 
 main() {
