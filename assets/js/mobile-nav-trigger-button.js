4ba3fad29c622385096022758f1ea6fde3299d6f
Pat Kelsh <pkelsh@gmail.com>
2020-10-07 12:10:20 -0400

    BCDA-3772 Static Site Glowup Header and Footer (#86)
    * BCDA-3772 updated header and footer. added build page
    
    * BCDA-3772 updated logo styles
    
    * BCDA-3772 fixed google group button issue
    
    * BCDA-3772 fixed header alignment

--- assets/js/mobile-nav-trigger-button.js
+++ assets/js/mobile-nav-trigger-button.js
@@ -1,14 +1,43 @@
-var acc = document.getElementsByClassName("mobile-nav-trigger-button");
-var i;
+var close_mobile_nav_button = document.getElementById("topnav__close-button"),
+    open_mobile_nav_button = document.getElementById("topnav__show-button"),
+    mobile_nav = document.getElementById("topnav-wrap"),
+    navbar = document.getElementById("navbar"),
+    overlay = document.createElement("div"),
+    nav_open_class = "nav-is-open",
+    visible_class = "is-visible",
+    overlay_class = "site-overlay";
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        var expanded = this.getAttribute('aria-expanded')
-        if (expanded === 'false') {
-          this.setAttribute('aria-expanded', 'true')
-        } else {
-          this.setAttribute('aria-expanded', 'false')
-        }
-    });
+function removeElementsByClass(className){
+  var elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+      elements[0].parentNode.removeChild(elements[0]);
+  }
 }
+
+
+
+if(typeof(close_mobile_nav_button) != 'undefined' && close_mobile_nav_button != null){
+  close_mobile_nav_button.addEventListener("click", function(e) {
+    document.body.classList.remove(nav_open_class);
+    mobile_nav.classList.remove(visible_class);
+    open_mobile_nav_button.focus();
+    removeElementsByClass(overlay_class);
+  });
+};
+
+if(typeof(open_mobile_nav_button) != 'undefined' && open_mobile_nav_button != null){
+  open_mobile_nav_button.addEventListener("click", function(e) {
+    document.body.classList.add(nav_open_class);
+    mobile_nav.classList.add(visible_class);
+    close_mobile_nav_button.focus();
+    navbar.insertBefore(overlay, mobile_nav);
+    overlay.classList.add(overlay_class);
+
+    setTimeout(function(){
+      overlay.classList.add(visible_class);
+    }, 100);
+
+    trapFocus(mobile_nav);
+  })
+};
 
