a8e744d0d7daf600f81e510c5003b79cc62bcd3d
Matt Snook <37818548+msnook@users.noreply.github.com>
2021-07-22 16:15:28 -0400

    Run Accessibility Scan in Github Actions CI (#135)

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -16,6 +16,15 @@ test() {
     docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
     sleep 20
     curl localhost:4000 | grep "Join the Google Group"
+    
+    # Perform accessibility scan
+    TARGET_TEST_ENV=http://host.docker.internal:4000
+    TARGETS_TO_SCAN="${TARGET_TEST_ENV}"
+    TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/guide.html"
+    TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/build.html"
+    TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/data.html"
+    TARGETS_TO_SCAN="${TARGETS_TO_SCAN} ${TARGET_TEST_ENV}/updates.html"
+    docker run --add-host=host.docker.internal:host-gateway --init --rm --cap-add=SYS_ADMIN orenfromberg/axe-puppeteer-ci:1.0.0@sha256:f83527a3ae8ab74088c001abfe44836946ba73f0afbbf460447f8a0c40281e70 ${TARGETS_TO_SCAN}
 }
 
 main() {
