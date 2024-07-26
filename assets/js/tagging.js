5864042d41899034a3d534158ad912f3ed25f6bb
Austin Canada <austincanada@navapbc.com>
2024-07-26 17:12:13 -0400

    BCDA-8262: Add Tealium code snippet

--- assets/js/tagging.js
+++ assets/js/tagging.js
@@ -2,7 +2,7 @@
 ---
 
 (function(a,b,c,d){
-  a='https://tags.tiqcdn.com/utag/cmsgov/cms-general/{{ jekyll.environment }}/utag.js';
+  a='https://tags.[first-party-domain].com/utag/cmsgov/cms-general/{{ jekyll.environment }}/utag.js';
   b=document;c='script';d=b.createElement(c);d.src=a;
   d.type='text/java'+c;d.async=true;
   a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
