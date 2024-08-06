dd3df584a88c2b67e1604bd2c78455edba42efca
Kevin Yeh <kevin@navapbc.com>
2024-08-06 15:41:15 -0400

    Update docker-compose commands (#209)
    ## 🎫 Ticket
    
    N/A
    
    ## 🛠 Changes
    
    - Did a find-replace for `docker-compose ` to `docker compose `
    
    ## ℹ️ Context
    
    See https://github.com/orgs/community/discussions/116610 for relevant
    discussion.
    
    ## 🧪 Validation
    
    [Stage
    deployment](https://management.bcda.cms.gov/job/BCDA%20-%20Deploy%20-%20Static%20Site/382/)

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -3,16 +3,16 @@
 set -eo pipefail
 
 docker_cleanup() {
-    docker-compose down
+    docker compose down
 }
 
 build() {
-    docker-compose -f docker-compose.yml build static_site
+    docker compose -f docker-compose.yml build static_site
 }
 
 test() {
     trap docker_cleanup EXIT
-    docker-compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
+    docker compose run -u "$(id -u "${USER}")":"$(id -g "${USER}")" --publish 4000:4000 --rm --entrypoint "bundle exec jekyll serve -H 0.0.0.0" -d static_site
     sleep 20
     curl localhost:4000 | grep "Join the Google Group"
 
