47baccc318259d813d198e437355dc12a368f12f
Austin Canada <austincanada@navapbc.com>
2024-11-14 13:17:41 -0500

    BCDA-8262: Add sync tag

--- assets/js/tagging.js
+++ assets/js/tagging.js
@@ -8,6 +8,12 @@
   a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
 })();
 
+(function(a,b,c,d){
+  a='https://tealium-tags.cms.gov/cms-general/{{ jekyll.environment }}/utag.sync.js';
+  b=document;c='script';d=b.createElement(c);d.src=a;
+  a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
+})();
+
 function metricTagging(category, action, label) {
     utag.link({ ga_event_category: category, ga_event_action: action, ga_event_label: label });
 };
