55d34b4c15b6e79848decfc92b258ea868937cea
Michael Valdes <michaelvaldes@navapbc.com>
2025-08-25 17:34:05 -0400

    Reorganize repo structure with jekyll dir

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,12 @@ const uswds = require("@uswds/compile");
 /** USWDS version **/
 uswds.settings.version = 3;
 
+uswds.paths.dist.theme = "./jekyll/sass";
+uswds.paths.dist.img = "./jekyll/assets/uswds/img";
+uswds.paths.dist.fonts = "./jekyll/assets/uswds/fonts"
+uswds.paths.dist.js = "./jekyll/assets/uswds/js";
+uswds.paths.dist.css = "./jekyll/assets/uswds/css";
+
 /** Exports **/
 // exports.init = uswds.init; // Use init only once
 exports.compile = uswds.compile;
